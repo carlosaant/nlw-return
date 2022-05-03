f2e4d83cf43d768d585498f6f09a79922549fa90
Carlos Antonio <carlos_antonio.minga@outlook.com>
2022-05-03 20:49:51 -0300

    configurado JS onscroll

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let navigation = document.getElementById('navigation');
+
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll');
+  } else {
+    navigation.classList.remove('scroll');
+  }
+}
